4285255e629dcb3df08772fea8aaadaef606a548
nikos <nikos.drongitis@spitogatos.gr>
2024-10-06 08:11:46 +0300

    Remove repeated quantity

--- src/components/Basket/BasketItem.vue
+++ src/components/Basket/BasketItem.vue
@@ -8,7 +8,7 @@
     <div class="flex items-center justify-between gap-6">
       <!-- Display the total price based on quantity -->
       <p class="unstyled !font-bold h2 md:h3">
-        ${{ $formatNumber(product.price * localQuantity) }}
+        ${{ $formatNumber(product.price * quantity) }}
       </p>
       <div
         class="flex rounded-lg overflow-hidden transition bg-accent/85 hover:bg-accent w-36 h-12 max-md:ml-auto justify-between items-center"
@@ -16,7 +16,7 @@
       >
         <!-- Display the quantity with sync between state and store -->
         <AddRemoveItems
-          :initialQuantity="localQuantity"
+          :initialQuantity="quantity"
           @quantityChange="handleQuantityChange"
         />
       </div>
@@ -33,7 +33,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, computed, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import AddRemoveItems from '@/components/Common/AddRemoveItems.vue'
 import ModalConfirm from './Modal.vue'
 import { useBasketStore } from '@/stores/basketStore'
@@ -57,11 +57,10 @@ const {
 const showModal = ref(false) // State for showing the modal
 const basketStore = useBasketStore() // Get basket store
 const genericStore = useGenericStore()
-const localQuantity = ref(quantity) // Local quantity to sync with AddRemoveItems
 
 onMounted(async () => {
   if (!genericStore.generic) {
-    await genericStore.loadGeneric() // Φορτώστε τα generic δεδομένα από το store
+    await genericStore.loadGeneric()
   }
 })
 
@@ -74,21 +73,12 @@ const dynamicConfirmMessage = computed(() => {
   return ''
 })
 
-// Watch for changes in the quantity prop and sync with localQuantity
-watch(
-  () => quantity,
-  (newQuantity) => {
-    localQuantity.value = newQuantity
-  },
-)
-
 // Function to update the quantity in the cart
 const handleQuantityChange = (newQuantity: number) => {
   if (newQuantity === 0) {
     showModal.value = true // Show the modal if quantity becomes 0
   } else {
     basketStore.updateQuantity(product.id, newQuantity)
-    localQuantity.value = newQuantity
   }
 }
 
@@ -101,7 +91,6 @@ const removeProduct = () => {
 // Function to keep the product with a quantity of 1
 const keepProduct = () => {
   basketStore.updateQuantity(product.id, 1) // Keep 1 product in the cart
-  localQuantity.value = 1 // Sync the quantity with AddRemoveItems
   showModal.value = false // Hide the modal
 }
 </script>
